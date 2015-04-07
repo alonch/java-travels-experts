8f015058a6f267b2cb24711f42b5448f3502fbd4
mbjae <mbjae_camacho@yahoo.com>
2015-04-07 16:24:45 -0600

    product class and test

---
+++ src/products/Product.java
@@ -0,0 +1,31 @@
+package products;
+
+public class Product {
+
+	private int ProductId;
+	private String ProdName="";
+	
+	public Product(String name) {
+		this.ProdName=name;
+	}
+	public Product() {
+	}
+	
+	public void setProdName(String name) {
+		this.ProdName=name;
+	}
+	public void setProductId(int id) {
+		this.ProductId=id;
+	}
+	public String getProdName() {
+		return this.ProdName;
+	}
+	public int getProductId() {
+		return this.ProductId;
+	}
+	public String toString(){
+		return ""+this.ProductId+
+				" "+this.ProdName;
+	}
+
+}
---
+++ test/products/test/ProductTest.java
@@ -0,0 +1,36 @@
+package products.test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import products.Product;
+
+
+public class ProductTest {
+
+	Product jet= new Product("Jet");;
+	Product car=new Product();;
+
+	@Test
+	public void createJet() {
+		//create product
+		//jet = new Product("Jet");
+		assertEquals("Jet", jet.getProdName());
+		assertNotNull(jet);
+	}
+	@Test
+	public void createCar(){
+		//car = new Product();
+		assertNotNull(car);
+		assertEquals("", car.getProdName());
+		
+	}
+	@Test
+	public void editProductAttribute(){
+		car.setProdName("new car");
+		assertEquals("new car", car.getProdName());
+		
+		car.setProductId(1);
+		assertEquals(1, car.getProductId());
+	}
+
+}
