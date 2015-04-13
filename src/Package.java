5170fdf037830095c56786a49fc4e73b9cc5b3d0
mbjae <mbjae_camacho@yahoo.com>
2015-04-13 14:45:09 -0600

    more classes
    Person
    Agent extends Person
    Customer extends Person
    Agency
    Package

---
+++ src/Package.java
@@ -0,0 +1,62 @@
+import java.util.Date;
+
+
+public class Package {
+	private int Id;
+	private String name;
+	private Date startDate;
+	private Date endDate;
+	private String description;
+	private double basePrice;
+	private double agencyCommission;
+	private boolean deleted;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getBasePrice() {
+		return basePrice;
+	}
+	public void setBasePrice(double basePrice) {
+		this.basePrice = basePrice;
+	}
+	public double getAgencyCommission() {
+		return agencyCommission;
+	}
+	public void setAgencyCommission(double agencyCommission) {
+		this.agencyCommission = agencyCommission;
+	}
+	public boolean isDeleted() {
+		return deleted;
+	}
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+}
---
+++ src/Person.java
@@ -0,0 +1,39 @@
+
+public class Person {
+	private int Id;
+	private String firstName;
+	private String lastName;
+	private String busPhone;
+	private String email;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBusPhone() {
+		return busPhone;
+	}
+	public void setBusPhone(String busPhone) {
+		this.busPhone = busPhone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
