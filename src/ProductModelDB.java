88fb8843be92e977a308b9c5490b326b1fdd2303
mbjae <mbjae_camacho@yahoo.com>
2015-04-13 13:16:44 -0600

    Product changes
    added productmodel
    renamed productsdb to productmodeldb

---
+++ src/ProductModelDB.java
@@ -0,0 +1,133 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+// Team 4 - Mehmet Demirci 
+
+public class ProductModelDB implements ProductModel {
+	
+	private Connection conn;
+	private Statement stmt;
+	private ResultSet rs;
+		
+	@Override
+	public void add(Product product) {
+		conn = TravelExpertsDB.GetConnection();
+		
+		try
+        {  
+			stmt = conn.createStatement();
+
+            String strSQL = "Insert Into products (ProdName) Values('" + product.getProdName() + "')";
+
+			if (stmt.executeUpdate(strSQL) == 0)
+			{
+			   System.out.println("no rows inserted");
+			}
+                        
+            conn.close();  
+        }
+        catch (Exception excp)
+        {
+            excp.printStackTrace();
+        }
+	}
+	
+	@Override
+	public void save(Product product) {
+		conn = TravelExpertsDB.GetConnection();
+		
+		try
+	    { 
+	      String query = "update products set ProdName = ? where ProductId = ?";
+	      PreparedStatement preparedStmt = conn.prepareStatement(query);
+	      preparedStmt.setString(1, product.getProdName());	      
+	      preparedStmt.setInt   (2, product.getProductId());	      
+	      preparedStmt.executeUpdate();
+	       
+	      conn.close();
+	    }
+	    catch (Exception e)
+	    {
+	      System.err.println("Got an exception! ");
+	      System.err.println(e.getMessage());
+	    }
+	}
+
+	@Override
+	public Product get(int id) {
+		Product product = new Product();		
+		conn = TravelExpertsDB.GetConnection();
+		
+		try 
+		{
+			stmt = conn.createStatement();
+			String sql = "select * from products where ProductId = " + id;
+			rs = stmt.executeQuery(sql);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numCols = rsmd.getColumnCount();
+			while (rs.next())
+			{				
+				String[] strArr = new String[5];
+				for (int i=1; i<=numCols; i++)
+				{
+					strArr[i] = rs.getString(i);
+				}
+				product.setProductId(Integer.parseInt(strArr[1]));
+				product.setProdName(strArr[2]);				
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return product;
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public List<Product> get() {
+		ArrayList<Product> products = new ArrayList<Product>();
+		
+		conn = TravelExpertsDB.GetConnection();
+		
+		try {
+			stmt = conn.createStatement();
+			String sql = "select * from products";
+			rs = stmt.executeQuery(sql);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numCols = rsmd.getColumnCount();
+			while (rs.next())
+			{
+				Product product = new Product();
+				String[] strArr = new String[5];
+				for (int i=1; i<=numCols; i++)
+				{
+					strArr[i] = rs.getString(i);
+				}
+				product.setProductId(Integer.parseInt(strArr[1]));
+				product.setProdName(strArr[2]);
+				products.add(product);
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return products;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub	
+	}
+}
