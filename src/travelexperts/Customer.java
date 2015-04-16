f781c0a4d4f8633aa77bcb175628d3a33d54e29a
alonch <alonchb@gmail.com>
2015-04-16 11:39:00 -0600

    add customer by user and password

--- src/travelexperts/Customer.java
+++ src/travelexperts/Customer.java
@@ -1,70 +1,104 @@
 package travelexperts;
 
-
-public class Customer extends Person{
+public class Customer extends Person {
 	//private int Id;
-	//private String firstName;
-	private String middleInitial;
-	//private String lastName;
-	private String address;
-	private String city;
-	private String province;
-	private String postal;
-	private String country;
-	private String homePhone;
+    //private String firstName;
+
+    private String middleInitial;
+    //private String lastName;
+    private String address;
+    private String city;
+    private String province;
+    private String postal;
+    private String country;
+    private String homePhone;
 	//private String busPhone;
-	//private String email;
-	private int agentId;
-	
-	public String getMiddleInitial() {
-		return middleInitial;
-	}
-	public void setMiddleInitial(String middleInitial) {
-		this.middleInitial = middleInitial;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getProvince() {
-		return province;
-	}
-	public void setProvince(String province) {
-		this.province = province;
-	}
-	public String getPostal() {
-		return postal;
-	}
-	public void setPostal(String postal) {
-		this.postal = postal;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getHomePhone() {
-		return homePhone;
-	}
-	public void setHomePhone(String homePhone) {
-		this.homePhone = homePhone;
-	}
-	public int getAgentId() {
-		return agentId;
-	}
-	public void setAgentId(int agentId) {
-		this.agentId = agentId;
-	}
-	public String toString(){
-		return super.getFirstName()+" "+super.getLastName();
-	}
+    //private String email;
+    private int agentId;
+    private String userid;
+    private String password;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public int getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(int agentId) {
+        this.agentId = agentId;
+    }
+
+    public String toString() {
+        return super.getFirstName() + " " + super.getLastName();
+    }
 }
--- src/travelexperts/CustomerModelDB.java
+++ src/travelexperts/CustomerModelDB.java
@@ -1,4 +1,5 @@
 package travelexperts;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,101 +8,127 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class CustomerModelDB implements CustomerModel {
 
-	private Connection conn;
-	private Statement stmt;
-	private ResultSet rs;
-	
-	@Override
-	public void save(Customer customer) {
-		try
-	    {
-			conn = TravelExpertsDB.GetConnection();
-			String query = "update customers "
-					+ "set AgentId = "+customer.getAgentId()
-					+ " where CustomerId = "+customer.getId();
-			PreparedStatement preparedStmt = conn.prepareStatement(query);      
-			preparedStmt.executeUpdate();
-	       
-			conn.close();
-	    }
-	    catch (Exception e)
-	    {
-	      System.err.println("Got an exception! ");
-	      System.err.println(e.getMessage());
-	    }
-	}
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet rs;
 
-	@Override
-	public List<Customer> get(int agentId) {
-		ArrayList<Customer> customers = null;
-		
-		try {
-			conn = TravelExpertsDB.GetConnection();
-			stmt = conn.createStatement();
-			String sql = "select * from customers where AgentId="+agentId;
-			rs = stmt.executeQuery(sql);
-			customers = new ArrayList<Customer>();
-			while (rs.next())
-			{
-				Customer customer= new Customer();
-				customer.setId(rs.getInt("CustomerId"));
-				customer.setFirstName(rs.getString("CustFirstName"));
-				customer.setLastName(rs.getString("CustLastName"));
-				customer.setAddress(rs.getString("CustAddress"));
-				customer.setCity(rs.getString("CustCity"));
-				customer.setProvince(rs.getString("CustProv"));
-				customer.setPostal(rs.getString("CustPostal"));
-				customer.setCountry(rs.getString("CustCountry"));
-				customer.setHomePhone(rs.getString("CustHomePhone"));
-				customer.setBusPhone(rs.getString("CustBusPhone"));
-				customer.setEmail(rs.getString("CustEmail"));
-				customer.setAgentId(rs.getInt("AgentId"));
-				
-				customers.add(customer);
-			}
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return customers;
-	}
-	@Override
-	public List<Customer> get() {
-		ArrayList<Customer> customers = null;
-		
-		try {
-			conn = TravelExpertsDB.GetConnection();
-			stmt = conn.createStatement();
-			String sql = "select * from customers";
-			rs = stmt.executeQuery(sql);
-			customers = new ArrayList<Customer>();
-			while (rs.next())
-			{
-				Customer customer= new Customer();
-				customer.setId(rs.getInt("CustomerId"));
-				customer.setFirstName(rs.getString("CustFirstName"));
-				customer.setLastName(rs.getString("CustLastName"));
-				customer.setAddress(rs.getString("CustAddress"));
-				customer.setCity(rs.getString("CustCity"));
-				customer.setProvince(rs.getString("CustProv"));
-				customer.setPostal(rs.getString("CustPostal"));
-				customer.setCountry(rs.getString("CustCountry"));
-				customer.setHomePhone(rs.getString("CustHomePhone"));
-				customer.setBusPhone(rs.getString("CustBusPhone"));
-				customer.setEmail(rs.getString("CustEmail"));
-				customer.setAgentId(rs.getInt("AgentId"));
-				
-				customers.add(customer);
-			}
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return customers;
-	}
+    @Override
+    public void save(Customer customer) {
+        try {
+            conn = TravelExpertsDB.GetConnection();
+            String query = "update customers "
+                    + "set AgentId = " + customer.getAgentId()
+                    + " where CustomerId = " + customer.getId();
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.executeUpdate();
+
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Customer> get(int agentId) {
+        ArrayList<Customer> customers = null;
+
+        try {
+            conn = TravelExpertsDB.GetConnection();
+            stmt = conn.createStatement();
+            String sql = "select * from customers where AgentId=" + agentId;
+            rs = stmt.executeQuery(sql);
+            customers = new ArrayList<Customer>();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getInt("CustomerId"));
+                customer.setFirstName(rs.getString("CustFirstName"));
+                customer.setLastName(rs.getString("CustLastName"));
+                customer.setAddress(rs.getString("CustAddress"));
+                customer.setCity(rs.getString("CustCity"));
+                customer.setProvince(rs.getString("CustProv"));
+                customer.setPostal(rs.getString("CustPostal"));
+                customer.setCountry(rs.getString("CustCountry"));
+                customer.setHomePhone(rs.getString("CustHomePhone"));
+                customer.setBusPhone(rs.getString("CustBusPhone"));
+                customer.setEmail(rs.getString("CustEmail"));
+                customer.setAgentId(rs.getInt("AgentId"));
+
+                customers.add(customer);
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return customers;
+    }
+
+    @Override
+    public List<Customer> get() {
+        ArrayList<Customer> customers = null;
+
+        try {
+            conn = TravelExpertsDB.GetConnection();
+            stmt = conn.createStatement();
+            String sql = "select * from customers";
+            rs = stmt.executeQuery(sql);
+            customers = new ArrayList<Customer>();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getInt("CustomerId"));
+                customer.setFirstName(rs.getString("CustFirstName"));
+                customer.setLastName(rs.getString("CustLastName"));
+                customer.setAddress(rs.getString("CustAddress"));
+                customer.setCity(rs.getString("CustCity"));
+                customer.setProvince(rs.getString("CustProv"));
+                customer.setPostal(rs.getString("CustPostal"));
+                customer.setCountry(rs.getString("CustCountry"));
+                customer.setHomePhone(rs.getString("CustHomePhone"));
+                customer.setBusPhone(rs.getString("CustBusPhone"));
+                customer.setEmail(rs.getString("CustEmail"));
+                customer.setAgentId(rs.getInt("AgentId"));
+
+                customers.add(customer);
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return customers;
+    }
+
+    @Override
+    public Customer getByUserIdAndPassword(String userId, String password) throws Exception{
+        try {
+            conn = TravelExpertsDB.GetConnection();
+            stmt = conn.createStatement();
+            String sql = "select * from customers where userid='" + userId +"' and password=password('"+password+"');";
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getInt("CustomerId"));
+                customer.setFirstName(rs.getString("CustFirstName"));
+                customer.setLastName(rs.getString("CustLastName"));
+                customer.setAddress(rs.getString("CustAddress"));
+                customer.setCity(rs.getString("CustCity"));
+                customer.setProvince(rs.getString("CustProv"));
+                customer.setPostal(rs.getString("CustPostal"));
+                customer.setCountry(rs.getString("CustCountry"));
+                customer.setHomePhone(rs.getString("CustHomePhone"));
+                customer.setBusPhone(rs.getString("CustBusPhone"));
+                customer.setEmail(rs.getString("CustEmail"));
+                customer.setAgentId(rs.getInt("AgentId"));
+                customer.setUserid(rs.getString("userid"));
+                return customer;
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw new Exception("user does not exits");
+    }
 }
